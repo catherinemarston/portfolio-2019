816b921c81e4b979e5d85f719488c5a1e8bc13b4
Catherine Marston <cathymarstonang@gmail.com>
2019-02-14 16:02:17 +0000

    added tween max animation, added svg, added base css classes, added spectre.css library

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CatherinePortfolio
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.1. It has tween max added.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import {Power1, Bounce} from 'gsap/all';
+
+declare var TweenMax: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  @ViewChild('sun') box: ElementRef;
+
+  ngOnInit(): void {
+    this.doIt();
+  }
+
+  doIt(): void {
+    TweenMax.fromTo(this.box.nativeElement, 2, {x: 20}, {x: 440, ease: Power1.easeOut});
+    TweenMax.fromTo(this.box.nativeElement, 2, {y: 20}, {y: 440, ease: Bounce.easeOut});
+  }
 }
